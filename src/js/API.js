72e48729a7d5b6840d9ed785dfc7ad5e683d55fa
vladkasyan <voadkasyan.2006@gmail.com>
2023-07-26 19:15:03 +0300

    Hm

---
+++ src/js/API.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+
+
+export default class NewsApiService {
+    constructor() {
+      this.searchQuery = '';
+      this.page = 1;
+      this.PER_PAGE = 40;
+    }
+    async fetchGallery() {
+      const axiosOptions = {
+        method: 'get',
+        url: 'https://pixabay.com/api/',
+        params: {
+          key: '38479962-70ceacf3a4add039bf0a1afc1',
+          q: `${this.searchQuery}`,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          page: `${this.page}`,
+          per_page: `${this.PER_PAGE}`,
+        },
+      };
+      try {
+        const response = await axios(axiosOptions);
+  
+        const data = response.data;
+  
+        this.incrementPage();
+        return data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  
+    incrementPage() {
+      this.page += 1;
+    }
+  
+    resetPage() {
+      this.page = 1;
+    }
+  
+    resetEndOfHits() {
+      this.endOfHits = false;
+    }
+  
+    get query() {
+      return this.searchQuery;
+    }
+  
+    set query(newQuery) {
+      this.searchQuery = newQuery;
+    }
+  }
+
+  
